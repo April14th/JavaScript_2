0ba216f306fd764e8b566c0bab95c10f224fb026
April14th <Kruspe@bk.ru>
2020-07-14 21:20:40 +0700

    ДЗ_1 выполнено

--- lesson_1/js/main.js
+++ lesson_1/js/main.js
@@ -1,21 +1,23 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', picture: 'notebook.jpg', price: 2000},
+    {id: 2, title: 'Mouse', picture: 'mouse.jpg', price: 20},
+    {id: 3, title: 'Keyboard', picture: 'keyboard.jpg', price: 200},
+    {id: 4, title: 'Gamepad', picture: 'gamepad.jpg', price: 50},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title = 'Наименование товара', picture = 'Изображение товара', price = 'Цена товара') => {
     return `<div class="product-item">
                 <h3>${title}</h3>
-                <p>${price}</p>
+                <img src="img/${picture}" alt="picture" width="204.8" height="156.3">
+                <p>${price} RUB</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
+
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item.title, item.picture, item.price));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
 
 renderPage(products);
