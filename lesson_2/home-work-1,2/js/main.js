87054681a86b8956be334176cefaa1b5cf2ee433
April14th <Kruspe@bk.ru>
2020-07-18 22:32:58 +0700

    Задание №1, 2 выполнено

--- lesson_2/home-work-1,2/js/main.js
+++ lesson_2/home-work-1,2/js/main.js
@@ -109,29 +109,3 @@ class BasketItem {
 
 const list = new ProductList();
 const basket = new Basket();
-
-
-
-// const products = [
-//     {id: 1, title: 'Notebook', picture: 'notebook.jpg', price: 2000},
-//     {id: 2, title: 'Mouse', picture: 'mouse.jpg', price: 20},
-//     {id: 3, title: 'Keyboard', picture: 'keyboard.jpg', price: 200},
-//     {id: 4, title: 'Gamepad', picture: 'gamepad.jpg', price: 50},
-// ];
-
-// const renderProduct = (title = 'Наименование товара', picture = 'Изображение товара', price = 'Цена товара') => {
-//     return `<div class="product-item">
-//                 <h3>${title}</h3>
-//                 <img src="img/${picture}" alt="picture" width="204.8" height="156.3">
-//                 <p>${price} RUB</p>
-//                 <button class="buy-btn">Купить</button>
-//             </div>`
-// };
-
-// const renderPage = list => {
-//     const productsList = list.map(item => renderProduct(item.title, item.picture, item.price));
-//     console.log(productsList);
-//     document.querySelector('.products').innerHTML = productsList.join('');
-// };
-
-// renderPage(products);
