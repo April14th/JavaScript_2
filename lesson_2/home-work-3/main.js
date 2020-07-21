80381bddfd8ae6d3bf57d6b7c43a4e685468db66
April14th <Kruspe@bk.ru>
2020-07-22 01:16:43 +0700

    Задание №3 в работе

--- lesson_2/home-work-3/main.js
+++ lesson_2/home-work-3/main.js
@@ -2,36 +2,85 @@ class Hamburger {
     constructor(size, stuffing) {
         this.size = size;
         this.stuffing = stuffing;
+        this.toppingsList = [];
+        this.getSize();
+        this.getStuffing();
+        this.calculateСharacteristicOfHamburger();
     }
 
     getSize() {
         const sizeButtons = document.querySelectorAll('.size');
-        sizeButtons.forEach(function(sizeButton) {
-            sizeButton.addEventListener('click', function(event) {
+        sizeButtons.forEach(function (sizeButton) {
+            sizeButton.addEventListener('click', function (event) {
                 hamburger.size = event.srcElement.dataset.size;
 
             });
         });
     }
-    
+
     getStuffing() {
         const stuffingButtons = document.querySelectorAll('.stuffing');
-        stuffingButtons.forEach(function(stuffingButton) {
-            stuffingButton.addEventListener('click', function(event) {
+        stuffingButtons.forEach(function (stuffingButton) {
+            stuffingButton.addEventListener('click', function (event) {
                 hamburger.stuffing = event.srcElement.dataset.stuffing;
             });
         });
     }
 
-    calculatePrice() {
+    calculateСharacteristicOfHamburger() {
         const calcBtn = document.querySelector('.calc-btn');
-        calcBtn.addEventListener('click', function() {
-           let priceOfSize = +document.querySelector('input[name="size"]:checked').dataset.price;
-           let priceOfStuffing = +document.querySelector('input[name="stuffing"]:checked').dataset.price;
-           hamburger.sumOfPrices = priceOfSize + priceOfStuffing;
+        this.calculatePrice(calcBtn);
+        this.calculateCalories(calcBtn);
+    }
+
+    calculatePrice(button) {
+        button.addEventListener('click', function () {
+            let priceFromSize = +document.querySelector('input[name="size"]:checked').dataset.price;
+            let priceFromStuffing = +document.querySelector('input[name="stuffing"]:checked').dataset.price;
+            hamburger.priceOfHamburger = priceFromSize + priceFromStuffing;
+        });
+    };
+
+    calculateCalories(button) {
+        button.addEventListener('click', function () {
+            let caloriesFromSize = +document.querySelector('input[name="size"]:checked').dataset.calorific;
+            let caloriesFromStuffing = +document.querySelector('input[name="stuffing"]:checked').dataset.calorific;
+            hamburger.calorificOfHamburger = caloriesFromSize + caloriesFromStuffing;
+        });
+    };
+
+    addTopping(topping, event) {
+        topping = event.srcElement.dataset.topping;
+        hamburger.toppingsList.push(topping);
+    }
+
+    removeTopping(topping, event) {
+        topping = event.srcElement.dataset.topping;
+        let indexTopping = hamburger.toppingsList.indexOf(topping);
+        hamburger.toppingsList.splice(indexTopping, 1);
+    }
+
+    getToppings(topping) {
+        const toppingButtons = document.querySelectorAll('.topping')
+        toppingButtons.forEach(function (toppingButton) {
+            toppingButton.addEventListener('click', function (event) {
+                if (toppingButton.checked) {
+                    hamburger.addTopping(topping, event);
+                } else if (toppingButton) {
+                    hamburger.removeTopping(topping, event);
+                };
+            });
         });
     }
 
+
+    // getToppings(topping) {
+    //     const toppingButtons = document.querySelectorAll('.topping');
+    //     toppingButtons.forEach(function(toppingButton) {
+    //         topping = toppingButton.getAttribute('data-topping');
+    //         hamburger.toppingsList.push(topping);
+    //     });
+    // };
 
     // addTopping(topping) {    // Добавить добавку }
     // removeTopping(topping) { // Убрать добавку }
@@ -43,6 +92,6 @@ class Hamburger {
 }
 
 const hamburger = new Hamburger();
-hamburger.getSize();
-hamburger.getStuffing();
-hamburger.calculatePrice();
+hamburger.getToppings();
+
+
