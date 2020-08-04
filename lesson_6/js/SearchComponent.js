0795e301e4acf06bcab6b2c7f8180436aba797eb
April14th <Kruspe@bk.ru>
2020-08-04 15:31:49 +0700

    продолжение

--- lesson_6/js/SearchComponent.js
+++ lesson_6/js/SearchComponent.js
@@ -1,3 +1,9 @@
 Vue.component('search-form', {
-
+    props: ['user-search'],
+    template: `<form action="#" class="search-form" @submit.prevent="$emit('filter')">
+                <input type="text" class="search-field" v-model="userSearch">
+                <button type="submit" class="btn-search">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>`
 });
