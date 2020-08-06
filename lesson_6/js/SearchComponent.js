8cbf6e8f33f32c369fd7c8db7c954a05048eb081
April14th <Kruspe@bk.ru>
2020-08-06 20:16:16 +0700

    вторая версия решения

--- lesson_6/js/SearchComponent.js
+++ lesson_6/js/SearchComponent.js
@@ -1,14 +1,28 @@
+// Vue.component('search-form', {
+//     props: ['value'],
+//     methods: {
+//       updateValue: function (value) {
+//         this.$emit("input", value);
+//       }
+//     },
+//     template: `<form action="#" class="search-form" @submit.prevent="$emit('filter')">
+//                 <input type="text" class="search-field" :value="value" @input="updateValue($event.target.value)">
+//                 <button type="submit" class="btn-search">
+//                     <i class="fas fa-search"></i>
+//                 </button>
+//             </form>`
+// });
+
 Vue.component('search-form', {
-    props: ['value'],
-    methods: {
-      updateValue: function (value) {
-        this.$emit("input", value);
-      }
-    },
-    template: `<form action="#" class="search-form" @submit.prevent="$emit('filter')">
-                <input type="text" class="search-field" :value="value" @input="updateValue($event.target.value)">
-                <button type="submit" class="btn-search">
-                    <i class="fas fa-search"></i>
-                </button>
-            </form>`
+  data () {
+    return {
+      userSearch: ''
+    }
+  },
+  template: `<form action="#" class="search-form" @submit.prevent="$parent.filter(userSearch)">
+              <input type="text" class="search-field" v-model="userSearch">
+              <button type="submit" class="btn-search">
+                  <i class="fas fa-search"></i>
+              </button>
+          </form>`
 });
--- lesson_6/js/main.js
+++ lesson_6/js/main.js
@@ -45,8 +45,8 @@ const app = new Vue({
                     }
                 })
         },
-        filter(){
-            let regexp = new RegExp(this.userSearch, 'i');
+        filter(userSearch){
+            let regexp = new RegExp(userSearch, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
