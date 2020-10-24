e765ecce885afcd2f73ab56cc8d892b44d8b2663
April14th <Kruspe@bk.ru>
2020-10-25 00:27:18 +0700

    сделан рендеринг каталога на Vue

--- lesson_5_alternative/src/index.js
+++ lesson_5_alternative/src/index.js
@@ -1,7 +1,24 @@
-import bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './layout/styles/styles.css';
+// import bootstrap from 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './layout/styles/styles.css';
 
-import app from './components';
+// import app from './components';
 
-app();
+// app();
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogItems: [],
+        catalogUrl: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json',
+        basketUrl: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/basket.json'
+    },
+    methods: {
+        get(url) {
+            return fetch(url).then(d => d.json());
+        }
+    },
+    mounted() {
+        this.get(this.catalogUrl).then(items => { this.catalogItems = items })
+    }
+})
