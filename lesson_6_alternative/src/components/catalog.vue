4fc831ee91d89bcffe5e94953c9f33a24255bd55
April14th <Kruspe@bk.ru>
2020-10-30 22:27:46 +0700

    ДЗ_6 выполнено

---
+++ lesson_6_alternative/src/components/catalog.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="row antirow2" id="catalog">
+        <item v-for="item of items" :key="item.productId" :item="item" :url="url" />
+    </div>
+</template>
+
+<script>
+import item from './item.vue';
+
+export default {
+    components: { item },
+
+    data() {
+        return {
+            // filer: '',
+            items: [],
+            filteredCatalogItems: [],
+            url: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json'
+        }
+    },
+
+    methods: {
+        get(url) {
+            return fetch(url).then(d => d.json());
+        },
+
+        // filterCatalog() {
+        //     this.filteredCatalogItems = this.items.filter(el => el.productName.match(new RegExp(this.filter, 'i')))
+        // }
+    },
+    
+    mounted() {
+        this.get(this.url).then(items => { this.items = items, this.filteredCatalogItems = items });
+    }
+}
+</script>
+
+<style>
+
+</style>
